fec434d188b53b785d887c246b3f0e5f7d1a08a0
Alexander Nohe <nohe@google.com>
2025-03-11 20:21:44 -0700

    Working sample

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import { GoogleAuth } from 'google-auth-library';
+import { inpaintingSample } from './sampleEditImg';
+import axios from 'axios';
+import fs from 'fs';
+
+const location = "us-central1";
+const project = 'lon-next';
+
+const generateUrl = (location: string, projectId: string): string => {
+  return `https://${location}-aiplatform.googleapis.com/v1/projects/${projectId}/locations/${location}/publishers/google/models/imagen-3.0-capability-001:predict`
+}
 
 const getAccessToken = async () => {
   const auth = new GoogleAuth();
@@ -11,7 +21,20 @@ const getAccessToken = async () => {
 
 async function main() {
   const token = await getAccessToken();
-  console.log(token.token);
+  // console.log(token.token);
+  const result = await axios.post(generateUrl(location, project), inpaintingSample, {
+    headers: {
+      "Authorization": `Bearer ${token.token}`,
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  });
+  console.log(result.statusText);
+  const predictions = result.data.predictions;
+  let i = 0;
+  for(const prediction of predictions) {
+    i++;
+    fs.writeFileSync(`pOf${i}.png`, new Buffer(prediction.bytesBase64Encoded, 'base64'));
+  }
 }
 
 main();
--- src/sampleEditImg.ts
+++ src/sampleEditImg.ts
@@ -1,18 +1,36 @@
 import { z } from 'zod';
 import { InpaintingSchema } from './schemas/inpainting';
+import fs from 'fs';
 
-const inpaintingSample: z.infer<typeof InpaintingSchema> = {
+const lm =fs.readFileSync('images/londonMask.png');
+const l = fs.readFileSync('images/london.jpg');
+
+const lmE = Buffer.from(lm).toString('base64');
+const lE = Buffer.from(l).toString('base64');
+
+export const inpaintingSample: z.infer<typeof InpaintingSchema> = {
   instances: [
     {
-      prompt: "Add strawberries",
+      prompt: "Add a big red london bus",
       referenceImages: [
         {
           referenceType: "REFERENCE_TYPE_RAW",
           referenceId: 1,
           referenceImage: {
-            bytesBase64Encoded: "asd"
+            bytesBase64Encoded: lE
           }
-        }
+        },
+        {
+          referenceType: "REFERENCE_TYPE_MASK",
+          referenceId: 2,
+          referenceImage: {
+            bytesBase64Encoded: lmE
+          },
+          maskImageConfig: {
+            maskMode: "MASK_MODE_USER_PROVIDED",
+            dilation: 0.1
+          }
+        },
       ]
     }
   ],
@@ -21,6 +39,6 @@ const inpaintingSample: z.infer<typeof InpaintingSchema> = {
       baseSteps: 35
     },
     editMode: "EDIT_MODE_INPAINT_INSERTION",
-    sampleCount: 4,
+    sampleCount: 2,
   },
 }
